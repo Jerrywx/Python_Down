4d35cf2e7e805c0b6b5f3b7928a323aa54a28afa
Jerrywx <23601145@qq.com>
2017-11-26 10:32:08 +0800

    Cookie

--- 01-Python/09-Django框架篇(59-69)/UserManager/UserManager/urls.py
+++ 01-Python/09-Django框架篇(59-69)/UserManager/UserManager/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from users import views
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^login', views.login),
+    url(r'^index', views.index),
+    url(r'^js_cookie', views.js_cookie),
 ]
--- 01-Python/09-Django框架篇(59-69)/UserManager/users/views.py
+++ 01-Python/09-Django框架篇(59-69)/UserManager/users/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from users import models
+from django.http import HttpResponse
+
+
+# 登录函数
+def login(request):
+
+    massage = ""
+    if request.method == "POST":
+
+        user = request.POST.get("username")
+        pwd = request.POST.get("password")
+
+        u = mm = models.Administrator.objects.filter(username=user, password=pwd)
+
+        if u:
+            rep = redirect('/index.html')
+            rep.set_cookie('username', user, max_age=60 * 3)
+            # rep.set_cookie('password', pwd)
+            return rep
+            # return render(request, 'index.html', {'username': user})
+        else:
+            massage = "用户名或密码错误!"
+
+    return render(request, 'login.html', {"msg":massage})
+
+
+#
+def index(request):
+    name = request.COOKIES.get("username")
+    if name:
+        return render(request, 'index.html', {'username': name})
+    else:
+        return redirect('/login.html')
+
+# jquery
+def js_cookie(req):
+    print("--------------------")
+    print(req.COOKIES)
+    print("--------------------")
+    # return HttpResponse("OK")
+    return render(req, 'js_cookie.html')
