9cc295aba57667a04fa1c49cdd38fff16e094575
Jerrywx <23601145@qq.com>
2018-02-01 12:00:10 +0800

    提交代码

--- 04-Tornado/04-SQLALchemy/03-one2more.py
+++ 04-Tornado/04-SQLALchemy/03-one2more.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy import create_engine
 
 # 链接数据库
-engine = create_engine('mysql+pymysql://root:123456@127.0.0.1:3306/test', echo=True)
+engine = create_engine('mysql+pymysql://root:123456@127.0.0.1:3306/test', echo=False)
 
 # 构造表
 Base = declarative_base()
@@ -42,29 +42,32 @@ class Father(Base):
 
 
 # 创建表
-Base.metadata.create_all(engine)
+# Base.metadata.create_all(engine)
 
 #
 Session = sessionmaker(bind=engine)
 session = Session()
 
 # ---------- 1. 插入数据
-# f1 = Father(name='alvin', age=50)
-# w1 = Son(name='little alvin1', age=4)
-# w2 = Son(name='little alvin2', age=5)
-#
+# f1 = Father(name='zhansan', age=50)
+# f2 = Father(name='lisi', age=50)
+# f3 = Father(name='wangwu', age=50)
+s1 = Son(name='little alvin1', age=4, father_id=2)
+s2 = Son(name='little alvin2', age=5, father_id=3)
+
 # f1.son = [w1, w2]
-# session.add(f1)
-# session.commit()
+# session.add_all([f1, f2, f3])
+session.add_all([s1, s2])
+session.commit()
 
 
 # ---------- 2. 插入数据
-f1 = Father(name="manman", age=26)
-s1 = Son(name="man-son", age=3, father_id=4)
-s2 = Son(name="man-son2", age=3, father_id=4)
+# f1 = Father(name="manman", age=26)
+# s1 = Son(name="man-son", age=3, father_id=4)
+# s2 = Son(name="man-son2", age=3, father_id=4)
 
-session.add_all([f1, s1, s2])
-session.commit()
+# session.add_all([f1, s1, s2])
+# session.commit()
 
 # fa = session.query(Father).filter(Father.name=='manman')[0]
 # print(fa.id)
